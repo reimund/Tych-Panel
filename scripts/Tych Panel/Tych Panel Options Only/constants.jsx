842b2f01466e7bbbcc26aaba839fa3e5158564bc
Reimund Trost <reimund@code7.se>
2011-03-19 17:34:57 +0100

    Added autosave and autoclose.

---
+++ scripts/Tych Panel/Tych Panel Options Only/constants.jsx
@@ -0,0 +1,21 @@
+﻿// Set defaults.
+var defaults = {
+	'padding': 8,
+	'resize': false,
+	'resize_width': 800,
+	'resample_method': 'bicubic',
+	'autosave': false,
+	'autoclose': false,
+	'save_directory': '~',
+	'filename': 'image',
+	'jpeg_quality': 12
+};
+
+var DIPTYCH_HORIZONTAL = 0;
+var DIPTYCH_PORTRAIT_LANDSCAPE_HORIZONTAL = 1;
+var DIPTYCH_LANDSCAPE_PORTRAIT_HORIZONTAL = 2;
+var TRIPTYCH_HORIZONTAL = 3;
+var TRIPTYCH_PORTRAIT_LANDSCAPE_GRID = 4;
+var TRIPTYCH_LANDSCAPE_PORTRAIT_GRID = 5;
+var QUAPTYCH_GRID = 6;
+var QUAPTYCH_HORIZONTAL = 7;
--- scripts/Tych Panel/Tych Panel Options.jsx
+++ scripts/Tych Panel/Tych Panel Options.jsx
@@ -1,19 +1,30 @@
 ﻿
+//@include Tych%20Panel%20Options%20Only/constants.jsx
 //@include Tych%20Panel%20Options%20Only/PSSettings.jsx
+
+// Load current settings.
+var tpSettings = {};
+var settings = new Settings();
+settings.setUID("TychPanelSettingsUniqueId");
+settings.setMSG("TychPanelSettings");
+settings.setType(SettingsType.SINGLE);
+settings.loadSettings();
+
+//settings.clearSettings();
+//settings.saveSettings();
+
+// Use stored settings if they exist, defaults otherwise.
+if (settings.numEntries() > 0)
+	tpSettings = settings.getEntryAt(0);
+else 
+	for (setting in defaults)
+		tpSettings[setting] = defaults[setting];
+
+
 createDialog();
 
 function createDialog()
 {
-	// Load current settings.
-	var settings = new Settings();
-	settings.setUID("TychPanelSettings");
-	settings.setMSG("TychPanelSettings");
-	settings.setType(SettingsType.SINGLE);
-	settings.loadSettings();
-
-	var padding = settings.getEntryByName('padding');
-	var defaultPadding = 8;
-
 
 	var dlg = new Window('dialog', 'Tych Panel options');
 
@@ -22,50 +33,119 @@ function createDialog()
 
 	dlg.mainGrp = dlg.add('group', undefined, 'Main');
 	dlg.mainGrp.orientation = 'column';
-	dlg.mainGrp.alignChildren = 'left';
+	dlg.mainGrp.alignChildren = 'fill';
 
 	dlg.generalOptions = dlg.mainGrp.add('panel', undefined, 'General');
-	dlg.generalOptions.alignChildren = "right";
 	dlg.generalOptions.paddingGrp = dlg.generalOptions.add('group');
-
 	dlg.generalOptions.resizeGrp = dlg.generalOptions.add('group');
-	dlg.generalOptions.resizeGrp.margins = [0, 15, 0, 0];
-	//dlg.generalOptions.resize = dlg.generalOptions.resizeGrp.add('checkbox', undefined, 'Resize generated images');
-	//dlg.generalOptions.tychWidth = dlg.generalOptions.add('group');
+	dlg.generalOptions.resize = dlg.generalOptions.resizeGrp.add('checkbox', undefined, 'Resize generated images');
+	dlg.generalOptions.resizeWidth = dlg.generalOptions.add('group');
 
 	with (dlg.generalOptions) {
+		alignChildren = "right";
+
+		resizeGrp.margins = [0, 15, 0, 0];
+		resize.value = tpSettings.resize;
+
 		paddingGrp.label = paddingGrp.add('statictext', undefined, 'Image spacing');
-		paddingGrp.input = paddingGrp.add('edittext', undefined, padding == null ? defaultPadding : padding);
+		paddingGrp.input = paddingGrp.add('edittext', undefined, tpSettings.padding);
 		paddingGrp.input.preferredSize = [40, 20];
 
-		//tychWidth.label = tychWidth.add('statictext', undefined, 'Target width');
-		//tychWidth.input = tychWidth.add('edittext', undefined, '800');
-		//tychWidth.input.preferredSize = [50, 20];
+		resizeWidth.label = resizeWidth.add('statictext', undefined, 'Target width');
+		resizeWidth.input = resizeWidth.add('edittext', undefined, tpSettings.resize_width);
+		resizeWidth.input.preferredSize = [50, 20];
+		resizeWidth.enabled = resize.value;
 	}
 
-	//dlg.outputOptions = dlg.mainGrp.add('panel', undefined, 'Output');
-	//dlg.outputOptions.alignChildren = "right";
+	dlg.output = dlg.mainGrp.add('panel', undefined, 'Output');
+	dlg.output.autosave = dlg.output.add('checkbox', undefined, 'Save generated images');
 
-	//dlg.autosave = dlg.outputOptions.add('checkbox', undefined, 'Autosave generated images');
-	//dlg.autoclose = dlg.outputOptions.add('checkbox', undefined, 'Close on save');
-	//dlg.autosave.value = false;
-	//dlg.autoclose.value = false;
+	dlg.output.directory = dlg.output.add('group');
+	dlg.output.directory.input = dlg.output.directory.add('edittext', undefined, new Folder(tpSettings.save_directory).fsName);
+	dlg.output.directory.browseButton = dlg.output.directory.add('button', undefined, 'Browse...');
+
+	dlg.output.filename = dlg.output.add('group');
+	dlg.output.filename.label = dlg.output.filename.add('statictext', undefined, 'File name');
+	dlg.output.filename.input = dlg.output.filename.add('edittext', undefined, tpSettings.filename);
+
+	dlg.output.quality = dlg.output.add('group');
+	dlg.output.quality.label = dlg.output.quality.add('statictext', undefined, 'JPEG Quality');
+	dlg.output.quality.slider = dlg.output.quality.add('slider', undefined, tpSettings.jpeg_quality);
+	dlg.output.quality.input = dlg.output.quality.add('edittext', undefined, tpSettings.jpeg_quality);
+
+	dlg.output.autoclose = dlg.output.add('checkbox', undefined, 'Close on save');
+
+	with (dlg.output) {
+		alignChildren = "right";
+		autosave.value = tpSettings.autosave;
+
+		autoclose.value = tpSettings.autoclose;
+		autoclose.enabled = autosave.value;
+
+		directory.enabled = autosave.value;
+		directory.input.preferredSize = [300, 20];
+
+		filename.enabled = autosave.value;
+		filename.input.preferredSize = [200, 20];
+
+		quality.enabled = autosave.value;
+		quality.slider.minvalue = 0;
+		quality.slider.maxvalue = 12;
+		quality.input.preferredSize = [35, 20];
+
+		directory.browseButton.onClick = function() {
+			directory.input.text = Folder.selectDialog('Please select a directory.').fsName;
+		};
+	}
 
 	dlg.buttonGrp = dlg.add('group', undefined, undefined);
 	dlg.buttonGrp.orientation = 'column';
 	dlg.okButton = dlg.buttonGrp.add('button', undefined, 'Ok');
 	dlg.cancelButton = dlg.buttonGrp.add('button', undefined, 'Cancel');
 
+
 	dlg.okButton.onClick = function() {
-		padding = Number(dlg.generalOptions.paddingGrp.input.text);
-		padding = isNaN(padding) ? defaultPadding : padding;
-		settings.addEntry('padding', padding);
+		// Get values from controls and put into the settings object.
+		tpSettings.padding = numOrDefault(dlg.generalOptions.paddingGrp.input.text, 'padding');
+		tpSettings.resize_width = numOrDefault(dlg.generalOptions.resizeWidth.input.text, 'resize_width');
+		tpSettings.resize = dlg.generalOptions.resize.value;
+		tpSettings.autosave =dlg.output.autosave.value;
+		tpSettings.autoclose =dlg.output.autoclose.value;
+		tpSettings.jpeg_quality = Math.round(dlg.output.quality.slider.value);
+		tpSettings.save_directory = dlg.output.directory.input.text;
+		tpSettings.filename = dlg.output.filename.input.text;
+
+		settings.addEntry('tp_settings', tpSettings);
 		settings.saveSettings();
+
 		dlg.close(1);
-	}
+	};
 
 	dlg.cancelButton.onClick = function() { dlg.close(2); }
+
+	dlg.generalOptions.resize.onClick = function() { dlg.generalOptions.resizeWidth.enabled = this.value; };
+
+	dlg.output.autosave.onClick = function() {
+		dlg.output.autoclose.enabled = this.value;
+		dlg.output.directory.enabled = this.value;
+		dlg.output.filename.enabled = this.value;
+		dlg.output.quality.enabled = this.value;
+	};
+
+	dlg.output.quality.slider.onChange = function() { dlg.output.quality.input.text = Math.round(this.value); }
+	dlg.output.quality.slider.onChanging = function() { dlg.output.quality.input.text = Math.round(this.value); }
+
+	dlg.output.quality.input.onChange = function() {
+		dlg.output.quality.slider.value = numOrDefault(Math.round(Number(this.text)), 'jpeg_quality');
+		this.text = dlg.output.quality.slider.value;
+	}
 
 	dlg.show(); 
 }
 
+
+function numOrDefault(str, name)
+{
+	var value = Number(str);
+	return isNaN(value) ? defaults[name] : value;
+}
