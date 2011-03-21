24f0e6adf1efe39a207b579a8c0abea7fc1d3eff
Reimund Trost <reimund@code7.se>
2011-03-21 10:33:40 +0100

    Added missing tpsettings.jsx file.

---
+++ scripts/Tych Panel/Tych Panel Options Only/tpsettings.jsx
@@ -0,0 +1,23 @@
+﻿//@include PSSettings.jsx
+
+var settings = new Settings();
+
+/**
+ * Loads settings.
+ */
+function tpGetSettings()
+{
+	var tpSettings = {};
+	settings.setUID("TychPanelSettingsUniqueId");
+	settings.setMSG("TychPanelSettings");
+	settings.setType(SettingsType.SINGLE);
+	settings.loadSettings();
+
+	if (settings.numEntries() > 0)
+		tpSettings = settings.getEntryAt(0);
+	else
+		for (setting in defaults)
+			tpSettings[setting] = defaults[setting];
+
+	return tpSettings;
+}
