e0f1849bfcca43516c904b4d18f1d58523492dd9
Reimund Trost <reimund@code7.se>
2011-03-27 23:34:45 +0200

    Crisper seams.

---
+++ scripts/Tych Panel/Helpers/tp_visitwww.jsx
@@ -0,0 +1,3 @@
+var plugin_dir = localize('$$$/LocalizedFilenames.xml/SourceDirectoryName/id/Extras/[LOCALE]/[LOCALE]_Plug-ins/value=Plug-ins');
+var html_file = app.path.toString() + "/" + plugin_dir + "/Panels/Tych Panel/content/index.html";
+File(html_file).execute();
