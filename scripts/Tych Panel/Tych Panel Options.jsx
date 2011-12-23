1551abd6b477ee665356147e782d51edae1cf0e7
Reimund Trost <reimund@code7.se>
2011-12-23 17:08:31 +0100

    Total rewrite of the Options window.

--- scripts/Tych Panel/Tych Panel Options.jsx
+++ scripts/Tych Panel/Tych Panel Options.jsx
@@ -16,195 +16,319 @@
 //settings.saveSettings();
 
 // Use stored settings if they exist, defaults otherwise.
-var tp_settings = tp_get_settings();
 
-create_dialog();
-
-function create_dialog()
+var TychOptions = function(tp_settings)
 {
-	var dialog = new Window('dialog', 'Tych Panel options');
-	var smallFont = ScriptUI.newFont(dialog.graphics.font.name, ScriptUI.FontStyle.REGULAR, 10);
+	var smallfont;
+				/*resize_width_group: Group { \
+					label: StaticText { text: 'Target width' }, \
+					input: EditText { \
+						text: '" + tp_settings.resize_width + "', \
+						preferredSize: [50, 20] \
+					}, \
+					enabled: " + tp_settings.resize + " \
+				}, \ */
+	var window_res = "dialog { \
+		orientation: 'row', \
+		alignChildren: 'top', \
+		main_group: Group { \
+			orientation: 'column', \
+			alignChildren: 'fill', \
+			general: Panel { \
+				text: 'General', \
+				alignChildren: 'right', \
+				spacing_group: Group { \
+					label: StaticText { text: 'Image spacing' } \
+					input: EditText { \
+						text: '" + tp_settings.spacing + "', \
+						preferredSize: [40, 20] \
+					} \
+				}, \
+				aspect_group: Group { \
+					keep_aspect: Checkbox { \
+						text: 'Keep aspect ratio on pictures', \
+						value: " + tp_settings.keep_aspect + " \
+					} \
+				}, \
+				aspect_text: StaticText { text: 'When disabled, images will be cropped as needed.' }, \
+				resize_label: Group { \
+					text: StaticText { text: 'Resize to fit' }, \
+					margins: [0, 20, 0, 0] \
+				}, \
+				resize_to_fit: Group { \
+					width_button: RadioButton { \
+						text: 'Width', \
+						value: " + tp_settings.fit_width + " \
+					}, \
+					height_button: RadioButton { \
+						text: 'Height', \
+						value: " + tp_settings.fit_height + " \
+					}, \
+					none_button: RadioButton { \
+						text: 'Don\\'t resize', \
+						value: " + (!tp_settings.fit_width && !tp_settings.fit_height) + " \
+					}, \
+				}, \
+				resize_width_group: Group { \
+					label: StaticText { text: 'Target width' } \
+					input: EditText { \
+						text: '" + tp_settings.resize_width + "', \
+						preferredSize: [50, 20] \
+					}, \
+				}, \
+				resize_height_group: Group { \
+					label: StaticText { text: 'Target height' } \
+					input: EditText { \
+						text: '" + tp_settings.resize_height + "', \
+						preferredSize: [50, 20] \
+					}, \
+				}, \
+				reorder_group: Group { \
+					margins: [0, 20, 0, 0] \
+					reorder: Checkbox { \
+						text: 'Enable reorder dialog', \
+						value: " + tp_settings.reorder + " \
+					} \
+				}, \
+				bridge_group: Group { \
+					use_bridge: Checkbox { \
+						text: 'Use selected images in Adobe Bridge', \
+						value: " + tp_settings.use_bridge_selection + " \
+					} \
+				} \
+				smart_object_group: Group { \
+					smart_object: Checkbox { \
+						text: 'Convert to smart objects.', \
+						value: " + tp_settings.convert_to_smartobject + " \
+					} \
+					margins: [0, 20, 0, 0] \
+				} \
+				layer_mask_group: Group { \
+					layer_mask: Checkbox { \
+						text: 'Mask layers', \
+						value: " + tp_settings.mask_layers + " \
+					}, \
+				}, \
+			}, \
+			compositing: Panel { \
+				text: 'Compositing', \
+				alignChildren: 'right', \
+				composite_group: Group { \
+					composite: Checkbox { \
+						text: 'Composite result', \
+						value: " + tp_settings.composite + " \
+					}, \
+				}, \
+				composite_text: StaticText { \
+					text: 'When enabled, the result will be combined with the previous open document.' \
+				}, \
+				maintain_label: Group { \
+					text: StaticText { text: 'When adding rows and columns, maintain' }, \
+					margins: [0, 20, 0, 0] \
+				}, \
+				maintain_group: Group { \
+					maintain_width: RadioButton { \
+						text: 'Width', \
+						value: " + tp_settings.maintain_width + ", \
+					}, \
+					maintain_height: RadioButton { \
+						text: 'Height', \
+						value: " + tp_settings.maintain_height + ", \
+					}, \
+					maintain_nothing: RadioButton { \
+						text: 'Nothing', \
+						value: " + (!tp_settings.maintain_width && !tp_settings.maintain_height) + ", \
+					}, \
+					enabled: " + tp_settings.composite + " \
+				}, \
+			}, \
+			output: Panel { \
+				text: 'Output', \
+				autosave: Checkbox { \
+					text: 'Save generated images', \
+					value: " + tp_settings.autosave + " \
+				}, \
+				directory: Group { \
+					input: EditText { \
+						text: '" + new Folder(tp_settings.save_directory).fsName + "', \
+						preferredSize: [300, 20] \
+					}, \
+					button: Button { text: 'Browse...' }, \
+					enabled: " + tp_settings.autosave + " \
+				}, \
+				filename: Group { \
+					label: StaticText { \
+						text: 'File name' \
+					}, \
+					input: EditText { \
+						text: '" + tp_settings.filename + "', \
+						preferredSize: [200, 20] \
+					}, \
+					enabled: " + tp_settings.autosave + " \
+				}, \
+				save_types: Group { \
+					jpeg: Checkbox { \
+						text: 'Jpeg', \
+						value: " + tp_settings.output_formats.jpg + ", \
+						enabled: " + tp_settings.autosave + ", \
+					}, \
+					psd: Checkbox { \
+						text: 'Psd', \
+						value: " + tp_settings.output_formats.psd + ", \
+						enabled: " + tp_settings.autosave + ", \
+ 					}, \
+					margins: [0, 20, 0, 0], \
+				}, \
+				quality: Group { \
+					label: StaticText { \ text: 'JPEG Quality' \ }, \
+					slider: Slider { \
+						value: '" + tp_settings.jpeg_quality + "', \
+						minvalue: 0, \
+						maxvalue: 12, \
+					}, \
+					input: EditText { \
+						text: '" + tp_settings.jpeg_quality + "', \
+						preferredSize: [35, 20], \
+					}, \
+				}, \
+				autoclose: Checkbox { \
+					text: 'Close on save', \
+					value: " + tp_settings.autoclose + ", \
+					enabled: " + tp_settings.autosave + " \
+				} \
+				alignChildren: 'right' \
+			}, \
+		}, \
+		button_group: Group { \
+			orientation: 'column', \
+			ok_button: Button { text: 'Ok' }, \
+			cancel_button: Button { text: 'Cancel' } \
+		}, \
+	}";
 
-	dialog.orientation = 'row';
-	dialog.alignChildren = 'top';
+	var w =  new Window(window_res, 'Tych Panel options');
 
-	dialog.main_group = dialog.add('group', undefined, 'Main');
-	dialog.main_group.orientation = 'column';
-	dialog.main_group.alignChildren = 'fill';
+	w.main_group.output.quality.enabled = w.main_group.output.autosave.value
+		&& w.main_group.output.jpeg.value;
 
-	// General section.
-	dialog.general = dialog.main_group.add('panel', undefined, 'General');
-	dialog.general.spacing_group = dialog.general.add('group');
-	dialog.general.aspect_group = dialog.general.add('group');
-	dialog.general.keepAspect = dialog.general.aspect_group.add('checkbox', undefined, 'Keep aspect ratio on images');
+	w.main_group.general.resize_width_group.input.enabled = w.main_group.general.resize_to_fit.width_button.value;
+	w.main_group.general.resize_height_group.input.enabled = w.main_group.general.resize_to_fit.height_button.value;
 
-	dialog.general.aspectText = dialog.general.add('statictext', undefined, 'When this is unchecked, images will be cropped to match height.');
-	dialog.general.aspectText.graphics.font = smallFont;
-	dialog.general.resize_group = dialog.general.add('group');
-	dialog.general.resize = dialog.general.resize_group.add('checkbox', undefined, 'Resize generated images');
-	dialog.general.resizeWidth = dialog.general.add('group');
-	dialog.general.smartobject_group = dialog.general.add('group');
-	dialog.general.smartobject = dialog.general.smartobject_group.add('checkbox', undefined, 'Convert to smart objects');
-	dialog.general.smartobject.value = tp_settings.convert_to_smartobject;
-	dialog.general.layermask_group = dialog.general.add('group');
-	dialog.general.layermask = dialog.general.layermask_group.add('checkbox', undefined, 'Mask layers');
-	dialog.general.layermask.value = tp_settings.mask_layers;
+	smallFont = ScriptUI.newFont(w.graphics.font.name, ScriptUI.FontStyle.REGULAR, 10);
+	w.main_group.general.aspect_text.graphics.font = smallFont;
+	w.main_group.compositing.composite_text.graphics.font = smallFont;
 
-	with (dialog.general) {
-		alignChildren = "right";
+	this.w = w;
+	this.tp_settings = tp_settings;
+	this.setup_events();
+}
 
-		keepAspect.value = tp_settings.keep_aspect;
+TychOptions.prototype.setup_events = function()
+{
+	var w = this.w;
+	var output = this.w.main_group.output;
+	var general = this.w.main_group.general;
+	var compositing = this.w.main_group.compositing;
+	var fit, maintain;
 
-		resize_group.margins = [0, 20, 0, 0];
-		resize.value = tp_settings.resize;
-
-		spacing_group.label = spacing_group.add('statictext', undefined, 'Image spacing');
-		spacing_group.input = spacing_group.add('edittext', undefined, tp_settings.spacing);
-		spacing_group.input.preferredSize = [40, 20];
-
-		resizeWidth.label = resizeWidth.add('statictext', undefined, 'Target width');
-		resizeWidth.input = resizeWidth.add('edittext', undefined, tp_settings.resize_width);
-		resizeWidth.input.preferredSize = [50, 20];
-		resizeWidth.enabled = resize.value;
-	}
-
-	// Just bring it section.
-	dialog.jbi = dialog.main_group.add('panel', undefined, 'Just Bring it');
-	dialog.jbi.alignChildren = "right";
-
-	reorder_group = dialog.jbi.add('group');
-	reorder_group.margins = [0, 10, 0, 0];
-	dialog.jbi.reorder = reorder_group.add('checkbox', undefined, 'Enable reorder dialog');
-	dialog.jbi.reorder.value = tp_settings.reorder;
-
-	bridge_group = dialog.jbi.add('group');
-	dialog.jbi.use_bridge = bridge_group.add('checkbox', undefined, 'Use Adobe Bridge selection');
-	dialog.jbi.use_bridge.value = tp_settings.use_bridge_selection;
-
-	// Output section.
-	dialog.output = dialog.main_group.add('panel', undefined, 'Output');
-	dialog.output.autosave = dialog.output.add('checkbox', undefined, 'Save generated images');
-
-	dialog.output.directory = dialog.output.add('group');
-	dialog.output.directory.input = dialog.output.directory.add('edittext', undefined, new Folder(tp_settings.save_directory).fsName);
-	dialog.output.directory.browseButton = dialog.output.directory.add('button', undefined, 'Browse...');
-
-	dialog.output.filename = dialog.output.add('group');
-	dialog.output.filename.label = dialog.output.filename.add('statictext', undefined, 'File name');
-	dialog.output.filename.input = dialog.output.filename.add('edittext', undefined, tp_settings.filename);
-
-	dialog.output.savetypes = dialog.output.add('group');
-	dialog.output.jpeg = dialog.output.savetypes.add('checkbox', undefined, 'Jpeg');
-	dialog.output.psd = dialog.output.savetypes.add('checkbox', undefined, 'Psd');
-
-	reorder_group.margins = [0, 10, 0, 0];
-	dialog.output.quality = dialog.output.add('group');
-	dialog.output.quality.label = dialog.output.quality.add('statictext', undefined, 'JPEG Quality');
-	dialog.output.quality.slider = dialog.output.quality.add('slider', undefined, tp_settings.jpeg_quality);
-	dialog.output.quality.input = dialog.output.quality.add('edittext', undefined, tp_settings.jpeg_quality);
-
-	dialog.output.autoclose = dialog.output.add('checkbox', undefined, 'Close on save');
-
-	with (dialog.output) {
-		alignChildren = "right";
-		autosave.value = tp_settings.autosave;
-
-		autoclose.value = tp_settings.autoclose;
-		autoclose.enabled = autosave.value;
-
-		directory.enabled = autosave.value;
-		directory.input.preferredSize = [300, 20];
-
-		filename.enabled = autosave.value;
-		filename.input.preferredSize = [200, 20];
-
-		savetypes.margins = [0, 20, 0, 0];
-		jpeg.value = tp_settings.output_formats.jpg;
-		jpeg.enabled = autosave.value;
-		psd.value = tp_settings.output_formats.psd;
-		psd.enabled = autosave.value;
-
-		quality.enabled = autosave.value && dialog.output.jpeg.value;
-		quality.slider.minvalue = 0;
-		quality.slider.maxvalue = 12;
-		quality.input.preferredSize = [35, 20];
-
-		directory.browseButton.onClick = function() {
-			directory.input.text = Folder.selectDialog('Please select a directory.').fsName;
-		};
-	}
-
-	dialog.button_group = dialog.add('group', undefined, undefined);
-	dialog.button_group.orientation = 'column';
-	dialog.okButton = dialog.button_group.add('button', undefined, 'Ok');
-	dialog.cancelButton = dialog.button_group.add('button', undefined, 'Cancel');
-
-
-	dialog.okButton.onClick = function()
+	general.resize_to_fit.width_button.onClick = function() 
 	{
-		// Get values from controls and put into the settings object.
-		tp_settings.spacing = num_or_default(dialog.general.spacing_group.input.text, 'spacing');
-		tp_settings.keep_aspect = dialog.general.keepAspect.value;
-		tp_settings.resize_width = num_or_default(dialog.general.resizeWidth.input.text, 'resize_width');
-		tp_settings.resize = dialog.general.resize.value;
-		tp_settings.convert_to_smartobject = dialog.general.smartobject.value;
-		tp_settings.mask_layers = dialog.general.layermask.value;
-		tp_settings.reorder = dialog.jbi.reorder.value;
-		tp_settings.use_bridge_selection = dialog.jbi.use_bridge.value;
-		tp_settings.autosave = dialog.output.autosave.value;
-		tp_settings.autoclose = dialog.output.autoclose.value;
-		tp_settings.output_formats.jpg = dialog.output.jpeg.value;
-		tp_settings.output_formats.psd = dialog.output.psd.value;
-		tp_settings.jpeg_quality = Math.round(dialog.output.quality.slider.value);
-		tp_settings.save_directory = dialog.output.directory.input.text;
-		tp_settings.filename = dialog.output.filename.input.text;
+		w.main_group.general.resize_width_group.input.enabled = w.main_group.general.resize_to_fit.width_button.value;
+		w.main_group.general.resize_height_group.input.enabled = false;
+	};
+
+	general.resize_to_fit.height_button.onClick = function() 
+	{
+		w.main_group.general.resize_height_group.input.enabled = w.main_group.general.resize_to_fit.height_button.value;
+		w.main_group.general.resize_width_group.input.enabled = false;
+	};
+
+	general.resize_to_fit.none_button.onClick = function() 
+	{
+		w.main_group.general.resize_width_group.input.enabled = false;
+		w.main_group.general.resize_height_group.input.enabled = false;
+	};
+
+	// Masks must be enabled with smart objects since it's the most convenient
+	// way to add seams. It doesn't really make sense to have smart objects
+	// without masks either.
+	general.smart_object_group.smart_object.onClick = function()
+	{
+		if (general.smart_object_group.smart_object.value)
+			general.layer_mask_group.layer_mask.value = general.smart_object_group.smart_object.value;
+		
+	};
+
+	// If smart objects is enabaled when turning masks off, smart objects will
+	// be disabled as well.
+	general.layer_mask_group.layer_mask.onClick = function()
+	{
+		if (!general.layer_mask_group.layer_mask.value)
+			general.smart_object_group.smart_object.value = general.layer_mask_group.layer_mask.value;
+	};
+
+	compositing.composite_group.composite.onClick = function()
+	{
+		compositing.maintain_group.enabled = this.value;
+	};
+
+	output.directory.button.onClick = function()
+	{
+		output.directory.input.text = Folder.selectDialog('Please select a directory.').fsName;
+	}
+
+	output.autosave.onClick = function()
+	{
+		output.autoclose.enabled = this.value;
+		output.directory.enabled = this.value;
+		output.filename.enabled = this.value;
+		output.save_types.jpeg.enabled = this.value;
+		output.save_types.psd.enabled = this.value;
+		output.quality.enabled = this.value && output.save_types.jpeg.value;
+	};
+
+	w.button_group.ok_button.onClick = function()
+	{
+		// Get values from controls and store them in the settings object.
+		tp_settings.spacing = num_or_default(general.spacing_group.input.text, 'spacing');
+		tp_settings.keep_aspect = general.aspect_group.keep_aspect.value;
+		tp_settings.fit_width = general.resize_to_fit.width_button.value
+		tp_settings.fit_height = general.resize_to_fit.height_button.value
+		tp_settings.resize_width = num_or_default(general.resize_width_group.input.text, 'resize_width');
+		tp_settings.resize_height = num_or_default(general.resize_height_group.input.text, 'resize_height');
+		tp_settings.convert_to_smartobject = general.smart_object_group.smart_object.value;
+		tp_settings.mask_layers = general.layer_mask_group.layer_mask.value;
+		tp_settings.reorder = general.reorder_group.reorder.value;
+		tp_settings.use_bridge_selection = general.bridge_group.use_bridge.value;
+		tp_settings.maintain_width = compositing.maintain_group.maintain_width.value;
+		tp_settings.maintain_height = compositing.maintain_group.maintain_height.value;
+		tp_settings.autosave = output.autosave.value;
+		tp_settings.autoclose = output.autoclose.value;
+		tp_settings.output_formats.jpg = output.save_types.jpeg.value;
+		tp_settings.output_formats.psd = output.save_types.psd.value;
+		tp_settings.jpeg_quality = Math.round(output.quality.slider.value);
+		tp_settings.save_directory = output.directory.input.text;
+		tp_settings.filename = output.filename.input.text;
 
 		settings.addEntry('tp_settings', tp_settings);
 		settings.saveSettings();
 
-		dialog.close(1);
+		w.close(1);
 	};
 
-	dialog.cancelButton.onClick = function()
-	{
-		dialog.close(2);
-	}
+	w.button_group.cancel_button.onClick = function() { w.close(2); };
+	//general.resize_group.resize.onClick = function() { general.resize_width_group.input.enabled = this.value; };
+	output.save_types.jpeg.onClick = function() { output.quality.enabled = this.value; };
+	output.quality.slider.onChange = function() { output.quality.input.text = Math.round(this.value); };
+	output.quality.slider.onChanging = function() { output.quality.input.text = Math.round(this.value); };
 
-	dialog.general.resize.onClick = function() { dialog.general.resizeWidth.enabled = this.value; };
-
-	dialog.output.autosave.onClick = function()
+	output.quality.input.onChange = function()
 	{
-		dialog.output.autoclose.enabled = this.value;
-		dialog.output.directory.enabled = this.value;
-		dialog.output.filename.enabled = this.value;
-		dialog.output.jpeg.enabled = this.value;
-		dialog.output.psd.enabled = this.value;
-		dialog.output.quality.enabled = this.value && dialog.output.jpeg.value;
+		output.quality.slider.value = num_or_default(Math.round(Number(this.text)), 'jpeg_quality');
+		this.text = output.quality.slider.value;
 	};
 
-	dialog.general.smartobject.onClick = function()
-	{
-		if (dialog.general.smartobject.value)
-			dialog.general.layermask.value = dialog.general.smartobject.value;
-	}
-
-	dialog.general.layermask.onClick = function()
-	{
-		if (!dialog.general.layermask.value)
-			dialog.general.smartobject.value = dialog.general.layermask.value;
-	}
-
-	dialog.output.jpeg.onClick = function() { dialog.output.quality.enabled = this.value; }
-	dialog.output.quality.slider.onChange = function() { dialog.output.quality.input.text = Math.round(this.value); }
-	dialog.output.quality.slider.onChanging = function() { dialog.output.quality.input.text = Math.round(this.value); }
-
-	dialog.output.quality.input.onChange = function()
-	{
-		dialog.output.quality.slider.value = num_or_default(Math.round(Number(this.text)), 'jpeg_quality');
-		this.text = dialog.output.quality.slider.value;
-	}
-
-	dialog.show(); 
+	this.w.show();
 }
 
 
@@ -213,3 +337,7 @@ function num_or_default(str, name)
 	var value = Number(str);
 	return isNaN(value) ? defaults[name] : value;
 }
+
+
+var tp_settings = tp_get_settings();
+var dialog = new TychOptions(tp_settings);
