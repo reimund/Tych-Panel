43e4501011d8f7ced0137e1fb344bc6c7f4868c6
Reimund Trost <reimund@code7.se>
2012-02-22 20:01:21 +0100

    Renamed visible scripts to more human friendly names.

---
+++ scripts/Tych Panel/Panel Helpers/New column (right).jsx
@@ -0,0 +1,2 @@
+$.evalFile(app.path + '/Presets/Scripts/Tych%20Panel/Tych%20Panel%20Only/tych.jsx');
+t.layout_and_composite(COLUMN, RIGHT);
--- scripts/Tych Panel/Panel Helpers/tp_new_column_right.jsx
+++
@@ -1,2 +0,0 @@
-$.evalFile(app.path + '/Presets/Scripts/Tych%20Panel/Tych%20Panel.jsx');
-t.layout_and_composite(COLUMN, RIGHT);
