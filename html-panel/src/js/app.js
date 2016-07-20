6d668f5a9d71a95b4f0926ecbe0d23701895f560
Reimund Trost <reimund@code7.se>
2016-07-20 15:59:42 +0200

    Implemented basic drag & drop.

--- html-panel/src/js/app.js
+++ html-panel/src/js/app.js
@@ -1,30 +1,70 @@
-var CSLibrary = new CSInterface();
+var CSLibrary = null;
+
+if (typeof(CSInterface) !== 'undefined')
+	CSLibrary = new CSInterface();
+
+var scripts = {
+	left: 'Tych Panel/Panel Helpers/Tych Panel - New column (left).jsx',
+	right: 'Tych Panel/Panel Helpers/Tych Panel - New column (right).jsx',
+	top:  'Tych Panel/Panel Helpers/Tych Panel - New row (top).jsx',
+	bottom: 'Tych Panel/Panel Helpers/Tych Panel - New row (bottom).jsx',
+	options: 'Tych Panel/Tych Panel - Options.jsx',
+};
 
 jQuery(document).ready(function($) {
-
-
 	$('.btn.options')
-		.on('click', function() { evalScript('Tych Panel/Tych Panel - Options.jsx'); });
+		.on('click', function() { evalScript(scripts.options); });
 
 	$('.btn.add-column-left')
-		.on('click', function() { runScript('Tych Panel/Panel Helpers/Tych Panel - New column (left).jsx'); });
+		.on('click', function() { runScript(scripts.left); });
 
 	$('.btn.add-column-right')
-		.on('click', function() { runScript('Tych Panel/Panel Helpers/Tych Panel - New column (right).jsx'); });
+		.on('click', function() { runScript(scripts.right); });
 
 	$('.btn.add-row-top')
-		.on('click', function() { runScript('Tych Panel/Panel Helpers/Tych Panel - New row (top).jsx'); });
+		.on('click', function() { runScript(scripts.top); });
 
 	$('.btn.add-row-bottom')
-		.on('click', function() { runScript('Tych Panel/Panel Helpers/Tych Panel - New row (bottom).jsx'); });
+		.on('click', function() { runScript(scripts.bottom); });
 
 	$('a.about')
 		.on('click', function() { CSLibrary.openURLInDefaultBrowser('http://lumens.se/tychpanel/'); });
 
-    CSLibrary.addEventListener("com.adobe.csxs.events.ThemeColorChanged", changeTheme); 
-    
-	changeTheme();
+	if (CSLibrary) {
+		CSLibrary.addEventListener('com.adobe.csxs.events.ThemeColorChanged', changeTheme); 
+
+		changeTheme();
+	}
 });
+
+function drop(compositeType, event) {
+	event.preventDefault();
+	
+	var files = event.dataTransfer.files
+	  , selectedFiles = []
+	  , selectedFilesScript = 'var selectedFiles = '
+	;
+	
+	for (var i in files) {
+		if (files.hasOwnProperty(i)) {
+			var path = files[i].path ? files[i].path : files[i].name;
+			
+			if (path)
+				selectedFiles.push(path);
+		}
+	};
+	
+	selectedFilesScript += JSON.stringify(selectedFiles) + ';';
+	
+	if (CSLibrary) {
+		var script = selectedFilesScript + evalFileScript(scripts[compositeType]);
+		setTimeout(function() { CSLibrary.evalScript(script) }, 100);
+	}
+}
+
+function allowDrop(event) {
+	event.preventDefault();
+}
 
 /**
  * Use showOpenDialog since File.openDialog behaves buggy when called via the
@@ -33,7 +73,7 @@ jQuery(document).ready(function($) {
 function runScript(scriptPath)
 {
 	var dialogResult  = null
-	  , selectedFiles = 'var selectedFiles = undefined;';
+	  , selectedFilesScript = 'var selectedFiles = undefined;';
 	;
 
 	CSLibrary.evalScript(getUseBridgeSelectionScript(), function(useBridgeSelection) {
@@ -42,38 +82,33 @@ function runScript(scriptPath)
 			dialogResult = window.cep.fs.showOpenDialog(true, false, 'Choose file(s) to add to composite', null);
 
 			if (dialogResult.err === window.cep.fs.NO_ERROR)
-				selectedFiles = 'var selectedFiles = ' + JSON.stringify(dialogResult.data) + ';'
+				selectedFilesScript = 'var selectedFiles = ' + JSON.stringify(dialogResult.data) + ';'
 		}
 
-		var script = selectedFiles + evalFileScript(scriptPath);
+		var script = selectedFilesScript + evalFileScript(scriptPath);
 
 		setTimeout(function() { CSLibrary.evalScript(script) }, 100);
 	});
-
 }
 
-function getUseBridgeSelectionScript()
-{
+function getUseBridgeSelectionScript() {
 	return script = evalFileScript('Tych Panel/Tych Panel Only/bridge.jsx')
 		+ 'useBridgeSelection;';
 }
 
-function evalFileScript(path)
-{
+function evalFileScript(path) {
 	return 'var scriptFile = new File(app.path + \'/Presets/Scripts/' + path + '\');'
 		+ '$.evalFile(scriptFile, 30000);';
 }
 
-function evalScript(path)
-{
+function evalScript(path) {
 	CSLibrary.evalScript(evalFileScript(path));
 }
 
 /**
  * This function will be called when PP's theme color been changed.
  */
-function changeTheme(event)
-{
+function changeTheme(event) {
 	var hostEnv = CSLibrary.getHostEnvironment();
 	var bgColor = toHex(hostEnv.appSkinInfo.panelBackgroundColor);
 
@@ -96,8 +131,7 @@ function changeTheme(event)
     
 }
 
-function toHex(color)
-{
+function toHex(color) {
     var red      = Math.round(color.color.red);
     var green    = Math.round(color.color.green);
     var blue     = Math.round(color.color.blue);
@@ -105,4 +139,3 @@ function toHex(color)
 
     return '#' + red.toString(16) + green.toString(16) + blue.toString(16);
 }
-
