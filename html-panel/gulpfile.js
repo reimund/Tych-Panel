c07c61f3cf02a8f94cb673ae71e15c6dae0b3f2d
Reimund Trost <reimund@code7.se>
2016-07-20 15:45:47 +0200

    Use Bower for dependencies.

--- html-panel/gulpfile.js
+++ html-panel/gulpfile.js
@@ -3,38 +3,54 @@
 var gulp         = require('gulp')
   , less         = require('gulp-less')
   , autoprefixer = require('gulp-autoprefixer')
-  , rename       = require('gulp-rename')
-  , notify       = require('gulp-notify')
+  , plug         = require('gulp-load-plugins')()
+  , runSequence  = require('run-sequence')
+  , bowerFiles   = require('main-bower-files')
+  , _            = require('lodash')
 ;
 
 var config = {
+	bower: 'bower_components/',
     styles: 'src/styles',
     panel: ['src/CSXS/**/*', 'src/images/*', 'src/js/*', 'src/Icon.png', 'src/index.html', 'src/.debug'],
 };
 
-gulp.task('dist', [], function() {
+var bowerConfig = {
+	paths: {
+		bowerDirectory: config.bower,
+		bowerrc: '.bowerrc',
+		bowerJson: 'bower.json'
+	}
+};
 
+gulp.task('dist', ['concat'], function() {
 	return gulp.src(config.panel, { base: 'src' })
 		.pipe(gulp.dest('dist'))
 	;
     //return gulp.src(config.styles + '/tychpanel.less')
 });
 
+gulp.task('concat', function() {
+	return gulp.src(bowerFiles(bowerConfig), { base: config.bower })
+		.pipe(plug.concat('bower.js'))	
+		.pipe(gulp.dest('dist/js'))
+	;
+});
+
 gulp.task('styles', function() {
     return gulp.src(config.styles + '/tychpanel.less')
-				.pipe(less({paths: config.styles}))
-				.pipe(autoprefixer('last 2 version'))
-				.pipe(rename({ basename: 'app' }))
-				.pipe(gulp.dest('dist/styles'))
-				.pipe(notify({ message: 'Styles task complete' }));
+		.pipe(less({ paths: config.styles }))
+		.pipe(autoprefixer('last 2 version'))
+		.pipe(plug.rename({ basename: 'app' }))
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(plug.notify({ message: 'Styles task complete' }))
+	;
 });
 
 gulp.task('serve', [], function() {
-
-	gulp.watch([
-			'./src/styles/**/*.less',
-		])
-		.on('change', 'styles');
+	gulp.watch(_.concat(['./src/styles/**/*.less', config.panel]))
+		.on('change', function() { runSequence(['styles', 'dist']); })
+	;
 });
 
 gulp.task('default', ['styles', 'dist'], function() {
